41407bb2cd5f75fd31cd3f0aec2937b599a7bd95
Seb Haan <sebhaan@gmail.com>
2023-09-18 18:28:37 +1000

    Add converter excel to json

--- tools/excel_json_converter.py
+++ tools/excel_json_converter.py
@@ -2,9 +2,78 @@
 
 import pandas as pd
 import os
+import json
 
 from load_schema_json import load_json, validate_json, json_to_dataframe
 from openpyxl.styles import PatternFill, Border, Side, Font
+
+def excel_to_json(excel_filename, json_filename_out):
+    """
+    Convert the Excel file to JSON data.
+
+    Parameters
+    ----------
+    excel_filename : str
+        Input Excel filename
+    json_filename : str
+        JSON filename
+    """
+    df = pd.read_excel(excel_filename)
+    data_dict = dataframe_to_json(df)
+
+    # write dict to json
+    data_json = json.dumps(data_dict, indent=2)
+
+    # replace NaN with None
+    data_json = data_json.replace('NaN', 'null')
+
+    # write json to file
+    with open(json_filename_out, 'w') as f:
+        f.write(data_json)
+
+     # validate the data
+    json_schema = "../schemas/schema_sequencing_examples_reason.json"
+    schema = load_json(json_schema)
+    data_loaded = load_json(json_filename_out)
+    assert validate_json(data_loaded, schema), "JSON data is invalid!"
+
+
+
+def dataframe_to_json(df):
+    """
+    converts dataframe to json
+
+    Parameters
+    ----------
+    df : pandas.DataFrame
+        dataframe to convert
+
+    Returns
+    -------
+    data : dict
+        JSON data
+    """
+    data = {}
+    data['Data'] = []
+    # group by type, subtype, subsubtype
+    df = df.groupby(['Type', 'Subtype', 'Sub_Subtype'])
+    # iterate through the groups
+    for name, group in df:
+        item = {}
+        item['Type'] = name[0]
+        item['Subtype'] = name[1]
+        item['Sub_Subtype'] = name[2]
+        item['Examples'] = []
+        # iterate through the rows in the group
+        for index, row in group.iterrows():
+            example = {}
+            example['Example'] = row['Example']
+            example['Reasoning'] = row['Reasoning']
+            example['Linkage_Word'] = row['Linkage_Word']
+            item['Examples'].append(example)
+        data['Data'].append(item)
+    return data
+
 
 def json_to_excel(
     json_filename, 
@@ -81,3 +150,13 @@ def test_json_to_excel():
     json_filename = "../schemas/sequencing_examples_reason.json"
     excel_filename = "../schemas/sequencing_examples_reason.xlsx"
     json_to_excel(json_filename, excel_filename)
+
+
+def test_excel_to_json():
+    """
+    Test json_to_excel().
+    """
+    
+    excel_filename = "../schemas/sequencing_examples_reason.xlsx"
+    json_filename_out = "../schemas/sequencing_examples_reason_recovered.json"
+    excel_to_json(excel_filename, json_filename_out)
