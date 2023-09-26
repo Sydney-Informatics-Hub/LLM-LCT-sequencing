6ec7d6085bbacc5525734e09a55bc3a486f1ff65
Hamish Croser <hamish.croser@sydney.edu.au>
2023-09-26 16:55:10 +1000

    Cleaned up styles for GUI

--- annotation/view/gui/widgets/styles.py
+++ annotation/view/gui/widgets/styles.py
@@ -1,11 +1,21 @@
-FIRST_CLAUSE_COLOUR = "#648FFF"
-SECOND_CLAUSE_COLOUR = "#DC267F"
-OVERLAP_COLOUR = "#5FA440"
-
 text_display_style = {
     "border-right": "1px solid black",
     "padding": "20px"
 }
+
+clause_stylesheet = """
+.first_clause {
+    background-color: rgba(100, 143, 255, 1)
+}
+
+.second_clause {
+    background-color: rgba(220, 38, 127, 1)
+}
+
+.clause_overlap {
+    background-color: rgba(95, 164, 64, 1)
+}
+"""
 
 curr_paragraph_style = {
     "color": "rgba(0, 0, 0, 1)",
@@ -20,3 +30,16 @@ context_paragraph_style = {
 controls_style = {
 
 }
+
+paragraph_heading_style = {
+
+}
+
+paragraph_id_style = {
+
+}
+
+controls_navigation_button_style = {
+
+}
+
