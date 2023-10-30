c284c307f8ebdfb8e541edea768da16eec9089de
Hamish Croser <hamish.croser@sydney.edu.au>
2023-10-30 14:11:08 +1100

    Added loading indicator widget to AnnotationViewWrapper.py

--- annotation/view/AnnotationViewWrapper.py
+++ annotation/view/AnnotationViewWrapper.py
@@ -1,9 +1,8 @@
 import panel as pn
 from panel import Row
 
-from annotation.controller.AnnotationController import AnnotationController
-from annotation.view.gui import Controls
-from annotation.view.gui import TextDisplay
+from annotation.controller import AnnotationController
+from annotation.view.gui import Controls, LoadingIndicator, TextDisplay
 
 pn.extension(notifications=True)
 
@@ -12,9 +11,12 @@ class AnnotationViewWrapper:
     def __init__(self, controller: AnnotationController):
         self.controller = controller
 
+        self.loading_indicator = LoadingIndicator(controller)
         self.text_display = TextDisplay(controller)
         self.controls = Controls(controller)
-        primary_row = Row(self.text_display.get_component(), self.controls.get_component())
+        primary_row = Row(self.loading_indicator.get_component(),
+                          self.text_display.get_component(),
+                          self.controls.get_component())
         self.layout = primary_row
 
         self.controller.update_displays()
