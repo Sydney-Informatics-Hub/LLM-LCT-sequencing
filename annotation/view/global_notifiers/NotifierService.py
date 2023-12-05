01e6edb5409fdf9f2b1c28ab6c22042c24f0e86b
Hamish Croser <hamish.croser@sydney.edu.au>
2023-12-05 16:01:02 +1100

    Success notifications disappear after 5 seconds instead of indefinitely

--- annotation/view/global_notifiers/NotifierService.py
+++ annotation/view/global_notifiers/NotifierService.py
@@ -5,7 +5,7 @@ class NotifierService:
     def notify_error(self, error_msg: str, duration: int = 0):
         panel.state.notifications.error(error_msg, duration)
 
-    def notify_success(self, success_msg: str, duration: int = 0):
+    def notify_success(self, success_msg: str, duration: int = 5000):
         panel.state.notifications.success(success_msg, duration)
 
     def notify_info(self, info_msg: str, duration: int = 5000):
