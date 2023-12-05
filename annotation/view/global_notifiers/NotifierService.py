955ee28b9add5eb99fc3a9dd6703cbff7fa6f31a
Hamish Croser <hamish.croser@sydney.edu.au>
2023-12-05 11:02:11 +1100

    Notifications are now persistent until dismissed

--- annotation/view/global_notifiers/NotifierService.py
+++ annotation/view/global_notifiers/NotifierService.py
@@ -3,7 +3,7 @@ import panel
 
 class NotifierService:
     def notify_error(self, error_msg: str):
-        panel.state.notifications.error(error_msg, duration=5000)
+        panel.state.notifications.error(error_msg, duration=0)
 
     def notify_success(self, success_msg: str):
-        panel.state.notifications.success(success_msg, duration=3000)
+        panel.state.notifications.success(success_msg, duration=0)
