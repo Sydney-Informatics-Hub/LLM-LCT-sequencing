e779bf4b1a3a9480fcadbb7c109b2301f5bde7da
Hamish Croser <hamish.croser@sydney.edu.au>
2023-12-05 14:29:48 +1100

    Added duration parameters for notification calls and added clear_all method

--- annotation/view/global_notifiers/NotifierService.py
+++ annotation/view/global_notifiers/NotifierService.py
@@ -2,8 +2,11 @@ import panel
 
 
 class NotifierService:
-    def notify_error(self, error_msg: str):
-        panel.state.notifications.error(error_msg, duration=0)
+    def notify_error(self, error_msg: str, duration: int = 0):
+        panel.state.notifications.error(error_msg, duration)
 
-    def notify_success(self, success_msg: str):
-        panel.state.notifications.success(success_msg, duration=0)
+    def notify_success(self, success_msg: str, duration: int = 0):
+        panel.state.notifications.success(success_msg, duration)
+
+    def clear_all(self):
+        panel.state.notifications.clear()
