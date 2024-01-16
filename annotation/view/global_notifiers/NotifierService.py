67d1faed1858025b55f3d2169c294bcd38936e91
Hamish Croser <hamish.croser@sydney.edu.au>
2024-01-17 09:44:17 +1100

    Added None check in NotifierService in the case that the notifications module has not yet loaded when methods are called

--- annotation/view/global_notifiers/NotifierService.py
+++ annotation/view/global_notifiers/NotifierService.py
@@ -6,13 +6,17 @@ class NotifierService:
         panel.extension(notifications=True)
 
     def notify_error(self, error_msg: str, duration: int = 0):
-        panel.state.notifications.error(error_msg, duration)
+        if panel.state.notifications is not None:
+            panel.state.notifications.error(error_msg, duration)
 
     def notify_success(self, success_msg: str, duration: int = 5000):
-        panel.state.notifications.success(success_msg, duration)
+        if panel.state.notifications is not None:
+            panel.state.notifications.success(success_msg, duration)
 
     def notify_info(self, info_msg: str, duration: int = 5000):
-        panel.state.notifications.info(info_msg, duration)
+        if panel.state.notifications is not None:
+            panel.state.notifications.info(info_msg, duration)
 
     def clear_all(self):
-        panel.state.notifications.clear()
+        if panel.state.notifications is not None:
+            panel.state.notifications.clear()
