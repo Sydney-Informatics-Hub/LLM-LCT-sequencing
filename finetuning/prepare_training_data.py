2d00599950721cb801e364361b101a0a84c5cf4a
Seb Haan <sebhaan@gmail.com>
2024-04-30 16:43:33 +1000

    Add data validation and cost estimate

--- finetuning/prepare_training_data.py
+++ finetuning/prepare_training_data.py
@@ -1,13 +1,20 @@
 # prepare training data
 
 import pandas as pd
+import numpy as np
 import json
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
+import tiktoken
+from collections import defaultdict
 
+# File path settings
 file_path = '../../data/trainingdata.xlsx'
+outpath_train = '../../data/data_openai_train.jsonl'
+outpath_val = '../../data/data_openai_val.jsonl'
 
-### Data Read-in and Cleaning
+
+### 1. Data loading and cleaning
 
 data = pd.read_excel(file_path)
 
@@ -38,7 +45,7 @@ print(type_counts)
 
 
 
-### Split Data into Train and Test Set
+### 2. Split Data into Train and Test Set
 
 # split data into train and test set with sklearn, 80% train, 20% test, ensure that test set is balanced
 train_data, val_data = train_test_split(data, test_size=0.2, stratify=data['Types'], random_state=42)
@@ -78,7 +85,7 @@ train_data.to_excel('../../data/trainingdata_trainset.xlsx')
 val_data.to_excel('../../data/trainingdata_valset.xlsx')
 
 
-### Convert Train and Test data for OpenAI Finetuning
+### 3. Convert Train and Test data for OpenAI Finetuning
 
 # Define system prompt
 
@@ -161,17 +168,140 @@ def create_finetuning_examples(system, queries, responses, outpath):
         }
         examples.append(example)
 
-    with open(outpath, 'w') as file:
+    with open(outpath, 'w', encoding='utf-8') as file:
         for example in examples:
             file.write(json.dumps(example) + '\n')
 
     return examples
 
 
-examples_train = create_finetuning_examples(prompt_system, train_queries, train_responses, outpath='../../data/data_openai_train.json')
-examples_val = create_finetuning_examples(prompt_system, val_queries, val_responses, outpath='../../data/data_openai_val.json')
+dataset_train = create_finetuning_examples(prompt_system, train_queries, train_responses, outpath=outpath_train)
+dataset_val = create_finetuning_examples(prompt_system, val_queries, val_responses, outpath=outpath_val)
 
 
+### 4. Data validation and cost estimates for finetuning
+
+# Load the train dataset if not already loaded
+if 'dataset_train' not in locals():
+    with open(outpath_train, 'r', encoding='utf-8') as f:
+        lines = f.readlines()
+    dataset_train = [json.loads(line) for line in lines]
+
+# Load the val dataset
+if 'dataset_val' not in locals():
+    with open(outpath_val, 'r', encoding='utf-8') as f:
+        lines = f.readlines()
+    dataset_val = [json.loads(line) for line in lines]\\\\
+
+def validate_dataset(dataset):
+    format_errors = defaultdict(int)
+
+    for ex in dataset:
+        if not isinstance(ex, dict):
+            format_errors["data_type"] += 1
+            continue
+            
+        messages = ex.get("messages", None)
+        if not messages:
+            format_errors["missing_messages_list"] += 1
+            continue
+            
+        for message in messages:
+            if "role" not in message or "content" not in message:
+                format_errors["message_missing_key"] += 1
+            
+            if any(k not in ("role", "content", "name", "function_call") for k in message):
+                format_errors["message_unrecognized_key"] += 1
+            
+            if message.get("role", None) not in ("system", "user", "assistant", "function"):
+                format_errors["unrecognized_role"] += 1
+                
+            content = message.get("content", None)
+            function_call = message.get("function_call", None)
+            
+            if (not content and not function_call) or not isinstance(content, str):
+                format_errors["missing_content"] += 1
+        
+        if not any(message.get("role", None) == "assistant" for message in messages):
+            format_errors["example_missing_assistant_message"] += 1
+
+    if format_errors:
+        print("Found errors:")
+        for k, v in format_errors.items():
+            print(f"{k}: {v}")
+            return False
+    else:
+        return True
 
 
+def num_tokens_from_messages(messages, tokens_per_message=3, tokens_per_name=1, encoding_name = "cl100k_base"):
+    encoding = tiktoken.get_encoding(encoding_name)
+    num_tokens = 0
+    for message in messages:
+        num_tokens += tokens_per_message
+        for key, value in message.items():
+            num_tokens += len(encoding.encode(value))
+            if key == "name":
+                num_tokens += tokens_per_name
+    num_tokens += 3
+    return num_tokens
 
+def num_assistant_tokens_from_messages(messages, encoding_name = "cl100k_base"):
+    encoding = tiktoken.get_encoding(encoding_name)
+    num_tokens = 0
+    for message in messages:
+        if message["role"] == "assistant":
+            num_tokens += len(encoding.encode(message["content"]))
+    return num_tokens
+
+def print_distribution(values, name):
+    print(f"\n#### Distribution of {name}:")
+    print(f"min / max: {min(values)}, {max(values)}")
+    print(f"mean / median: {np.mean(values)}, {np.median(values)}")
+    print(f"p5 / p95: {np.quantile(values, 0.1)}, {np.quantile(values, 0.9)}")
+
+
+if validate_dataset(dataset_train):
+    print("Train dataset is valid!")
+
+if validate_dataset(dataset_val):
+    print("Validation dataset is valid!")
+
+# Calculate the number of tokens in each example
+n_missing_system = 0
+n_missing_user = 0
+n_messages = []
+convo_lens = []
+assistant_message_lens = []
+encoding = tiktoken.get_encoding("cl100k_base")
+
+for ex in dataset_train:
+    messages = ex["messages"]
+    if not any(message["role"] == "system" for message in messages):
+        n_missing_system += 1
+    if not any(message["role"] == "user" for message in messages):
+        n_missing_user += 1
+    n_messages.append(len(messages))
+    convo_lens.append(num_tokens_from_messages(messages))
+    assistant_message_lens.append(num_assistant_tokens_from_messages(messages))
+    
+print("Num examples missing system message:", n_missing_system)
+print("Num examples missing user message:", n_missing_user)
+print_distribution(n_messages, "num_messages_per_example")
+print_distribution(convo_lens, "num_total_tokens_per_example")
+print_distribution(assistant_message_lens, "num_assistant_tokens_per_example")
+n_too_long = sum(l > 4096 for l in convo_lens)
+print(f"\n{n_too_long} examples may be over the 4096 token limit, they will be truncated during fine-tuning")
+
+# Pricing and default n_epochs estimate
+MAX_TOKENS_PER_EXAMPLE = 4096
+
+n_epochs = 3
+cost_per_token = 0.008/1000 # For GPT-3.5-turbo
+
+n_billing_tokens_in_dataset = sum(min(MAX_TOKENS_PER_EXAMPLE, length) for length in convo_lens)
+print(f"Dataset has ~{n_billing_tokens_in_dataset} tokens that will be charged for during training")
+print(f"By default, you'll train for {n_epochs} epochs on this dataset")
+print(f"By default, you'll be charged for ~{n_epochs * n_billing_tokens_in_dataset} tokens")
+cost = cost_per_token * n_epochs * n_billing_tokens_in_dataset
+print(f'Estimated cost for training is {round(cost,2)} USD.')
